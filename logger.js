348e1389891ad1f85e1c12558d076631a63d1fdc
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-23 23:59:29 +0200

    hidden

--- logger.js
+++ logger.js
@@ -1,1 +1,9 @@
 console.log('Działam i mam się dobrze');
+
+let guzik = document.getElementById('guzik')
+guzik.addEventListener('click', showHidden);
+
+function showHidden() {
+  let hidden = document.getElementById('hidden');
+  hidden.hidden = false;
+}
