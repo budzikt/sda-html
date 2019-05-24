bac9c4b1a9e9b987719007fd95a73282019b55c0
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-24 12:54:14 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,11 @@
 console.log('Działam i mam się dobrze');
+
+document.getElementById('guzik')
+.addEventListener('click', () => {
+  let el = document.getElementById('mainDiv')
+  el.style.backgroundColor = 'red';
+  el.style.height = '150px';
+  el.style.width = '30%';
+  el.title = 'Mały czerwony prostokąt'
+  el.firstElementChild.remove();
+})
