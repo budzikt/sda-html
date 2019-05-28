551e3be1e792b65b7a7ee24d4f657ecd6b0fd0d8
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-28 13:22:15 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,13 @@
 console.log('Działam i mam się dobrze');
+
+function showHide(MouseEvent) {
+  let id = MouseEvent.srcElement.id
+  console.log(MouseEvent);
+  let el = document.getElementById(`div${id}`)
+  el.hidden = !el.hidden;
+  MouseEvent.srcElement.innerText = MouseEvent.srcElement.innerText == 'Ukryj' ? 'Pokaż' : 'Ukryj'
+}
+
+document.getElementById('1').addEventListener('click', showHide)
+document.getElementById('2').addEventListener('click', showHide)
+document.getElementById('3').addEventListener('click', showHide)
