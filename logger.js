4d0b8b6d6cb7cd7e04981f81123c297cc14850e6
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-28 11:14:14 +0200

    .

--- logger.js
+++ logger.js
@@ -3,9 +3,17 @@ console.log('Działam i mam się dobrze');
 document.getElementById('guzik')
 .addEventListener('click', () => {
   let el = document.getElementById('mainDiv')
-  el.style.backgroundColor = 'red';
+  el.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   el.style.height = '150px';
   el.style.width = '30%';
-  el.title = 'Mały czerwony prostokąt'
-  el.firstElementChild.remove();
+  el.style.border = 'solid';
+  el.style.borderColor  = 'red';
+
+  let childs = el.childElementCount;
+
+  if(el.firstElementChild.tagName !== 'BUTTON') {
+    el.firstElementChild.remove();
+  }
+
+  el.title = `Mały zielony prostokąt z ${childs} dziećmi`
 })
