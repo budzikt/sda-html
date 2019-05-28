3793a22e16ea36c0ab1b5560b6c537a61893c4d1
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-28 23:32:14 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,14 @@
 console.log('Działam i mam się dobrze');
+
+function appendDiv() {
+  let container = document.getElementById('parent');
+  let txt = document.getElementById('txt').value;
+
+  let child = document.createElement('div')
+  child.classList.add('sized', 'r');
+  child.innerHTML = txt;
+
+  container.appendChild(child);
+}
+
+document.getElementById('btn').addEventListener('click', appendDiv);
