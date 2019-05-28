72d57a837fa6bbed521b20da171078a1b2707cc6
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-28 12:40:00 +0200

    ..

--- logger.js
+++ logger.js
@@ -5,7 +5,9 @@ function showHidden() {
   let hidden = document.getElementById('hidden');
   let v1 = hidden.hidden;
   hidden.hidden = !v1;
+}
 
+function hideShown() {
   let shown = document.getElementById('shown');
   let v2 = shown.hidden;
   shown.hidden = !v2;
@@ -13,3 +15,4 @@ function showHidden() {
 
 let guzik = document.getElementById('guzik')
 guzik.addEventListener('click', showHidden);
+guzik.addEventListener('click', hideShown);
