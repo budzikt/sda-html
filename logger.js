63c1e5206c4923aa797d11e140bb6fb6927273ff
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-28 21:01:01 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,19 @@
 console.log('Działam i mam się dobrze');
+
+function toggleDisplay(el) {
+  el.style.display = el.style.display === 'none' ? 'block' : 'none' 
+}
+
+function toggleHodden(el) {
+  el.style.visibility = el.style.visibility === 'hidden' ? 'visible' : 'hidden';
+}
+
+document.getElementById('hidden').addEventListener('click', () => {
+  let div = document.getElementById('divHidden');
+  toggleHodden(div);
+})
+
+document.getElementById('display-none').addEventListener('click', () => {
+  let div = document.getElementById('divDisplay');
+  toggleDisplay(div);
+})
