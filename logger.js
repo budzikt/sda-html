91fd465a3717a1d921f350161791c0b56f75f63f
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-29 13:15:55 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,23 @@
 console.log('Działam i mam się dobrze');
+
+
+
+document.getElementById('marginLR').addEventListener('input', (e) => {
+  console.log(e.srcElement.value)
+  document.getElementById('innerDiv').style.marginLeft = '' + e.srcElement.value + 'em';
+})
+
+document.getElementById('marginUD').addEventListener('input', (e) => {
+  console.log(e.srcElement.value)
+  document.getElementById('innerDiv').style.marginTop = '' + e.srcElement.value + 'em';
+})
+
+document.getElementById('paddingR').addEventListener('input', (e) => {
+  console.log(e.srcElement.value)
+  document.getElementById('innerDiv').style.paddingLeft = '' + e.srcElement.value + 'em';
+})
+
+document.getElementById('paddingUD').addEventListener('input', (e) => {
+  console.log(e.srcElement.value)
+  document.getElementById('innerDiv').style.paddingTop = '' + e.srcElement.value + 'em';
+})
