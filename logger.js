7860608078147a8ff405bc1219714f6e1f1e94ef
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-30 09:33:57 +0200

    .

--- logger.js
+++ logger.js
@@ -4,12 +4,12 @@ console.log('Działam i mam się dobrze');
 
 document.getElementById('marginLR').addEventListener('input', (e) => {
   console.log(e.srcElement.value)
-  document.getElementById('innerDiv').style.marginLeft = '' + e.srcElement.value + 'em';
+  document.getElementById('innerDiv').style.marginLeft = '' + e.srcElement.value + '%';
 })
 
 document.getElementById('marginUD').addEventListener('input', (e) => {
   console.log(e.srcElement.value)
-  document.getElementById('innerDiv').style.marginTop = '' + e.srcElement.value + 'em';
+  document.getElementById('innerDiv').style.marginTop = '' + e.srcElement.value + '%';
 })
 
 document.getElementById('paddingR').addEventListener('input', (e) => {
@@ -21,3 +21,6 @@ document.getElementById('paddingUD').addEventListener('input', (e) => {
   console.log(e.srcElement.value)
   document.getElementById('innerDiv').style.paddingTop = '' + e.srcElement.value + 'em';
 })
+
+
+
