4312339f49a38d6da1c9c7e905f64cd3d3908954
Tomek Budzinski <tomaszb@connectpoint.local>
2019-05-31 21:58:36 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,8 @@
 console.log('Działam i mam się dobrze');
+
+if (
+  document.getElementById('pId').classList.contains('text-wysrodkowany') &&
+  document.getElementById('pId').classList.contains('text-czerwony')
+) {
+  document.getElementById('pId').innerHTML = 'Super!'
+}
