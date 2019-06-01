c0dbf8efcac763333e9b4a31cae230308835cffa
Tomek Budzinski <tomaszb@connectpoint.local>
2019-06-01 21:56:35 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,22 @@
 console.log('Działam i mam się dobrze');
+
+function assignVals(vals) {
+  
+}
+
+fetch('https://reqres.in/api/users?page=2')
+.then(resp => resp.json())
+.then(json => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(json);
+    }, 2500)
+  })
+})
+.then(v => {
+  let dynCont = document.getElementById('dynamiczny')
+  let arrElems = []
+  v.data.forEach(element => {
+    dynCont.appendChild(document.createElement('p')).innerHTML = element.email;
+  });
+})
