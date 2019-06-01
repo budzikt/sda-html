dd5bde9dcecc4c4192f3d4dd0e46d3fbc4fb5807
Tomek Budzinski <tomaszb@connectpoint.local>
2019-06-01 21:19:34 +0200

    .

--- logger.js
+++ logger.js
@@ -1,1 +1,15 @@
 console.log('Działam i mam się dobrze');
+
+document.getElementById('guzik').addEventListener('click', (ev) => {
+  let comment = document.getElementById('txt').value
+  let para = document.createElement("p");
+  para.innerHTML = comment;
+  document.getElementById('comments').appendChild(para);
+})
+
+document.getElementById('guzik2').addEventListener('click', (ev) => {
+  let comment = document.getElementById('txt2').value
+  let para = document.createElement("p");
+  para.innerHTML = comment;
+  document.getElementById('comments2').prepend(para);
+})
