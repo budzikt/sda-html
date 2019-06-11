78b77fe702fcb2331731e3e6e2c75977edf7706c
Tomek Budzinski <tomaszb@connectpoint.local>
2019-06-11 23:25:57 +0200

    .

---
+++ jsThisBind.js
@@ -0,0 +1,24 @@
+
+let o = {
+  a: 60
+}
+
+let o2 = {
+  a: 120
+}
+
+function wypisz() {
+  console.log(this.a);
+  return this.a
+}
+
+o.f = wypisz;
+o2.f = wypisz;
+
+o2.f();
+o.f();
+
+o.f2 = wypisz.bind(o2);
+o.f2();
+
+wypisz.apply(o2);
