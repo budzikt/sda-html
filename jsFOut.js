694f1a5145776c6141ff607ec7156f15c2463cb3
Tomek Budzinski <tomaszb@connectpoint.local>
2019-06-12 08:59:58 +0200

    .

---
+++ jsFOut.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+function dodajDo(baza) {
+  return function(add) {
+    let bazaD = baza;
+    return bazaD + add;
+  }
+}
+
+let dodajDo10 = dodajDo(10);
+console.log(dodajDo10(5));
+
+let dodajDo25 = dodajDo(25);
+console.log(dodajDo25(5));
