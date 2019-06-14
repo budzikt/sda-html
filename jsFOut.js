eda181006e2d483326f885bb080b155693a8e4a3
Tomek Budzinski <tomaszb@connectpoint.local>
2019-06-14 22:45:15 +0200

    .

--- jsFOut.js
+++ jsFOut.js
@@ -14,3 +14,20 @@ let dodajDo25 = dodajDo(25);
 
 console.log(dodajDo10(5));
 console.log(dodajDo25(5));
+
+function sekrecik(sekrecik, haslo) {
+  let _sekrecik = sekrecik;
+  let _haslo = haslo;
+  return function(hasloIn) {
+    if (hasloIn === _haslo) {
+      return _sekrecik;
+    } else {
+      return 'Nope.';
+    }
+  }
+}
+
+let sekrecikChecker = sekrecik('Slysze glosy', '1234');
+
+console.log(sekrecikChecker('niePasuje'))
+console.log(sekrecikChecker('1234'))
