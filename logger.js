9f8848734816eac3aa73aa991040159c95cf47e7
Tomek Budziński <budzikt@gmail.com>
2019-09-29 10:53:59 +0200

    fajne zmiany

--- logger.js
+++ logger.js
@@ -1,1 +1,11 @@
 console.log('Działam i mam się dobrze');
+
+function wypisz(cos) {
+    console.log('Wcisnieto guzik');
+    console.log(cos);
+}
+
+
+let a = document.getElementById('guzik');
+console.log(a);
+a.addEventListener('click', wypisz);
