a74c94c42daa44a6e6d0255cb099b50aaaa276e8
Tomek Budziński <budzikt@gmail.com>
2019-10-04 21:14:13 +0200

    wio

--- logger.js
+++ logger.js
@@ -1,1 +1,16 @@
 console.log('Działam i mam się dobrze');
+
+$("#id2").click(function(){
+    $('.toast').toast('show');
+  });
+
+let guziki = 5;
+
+for(var i = 1; i <= guziki; i++) {
+    var ele = document.getElementById(`id${i}`)
+    if (ele) {
+        ele.addEventListener('click', () => {
+            alert(`Wartość id guzika to ${i}`);
+        });
+    }
+}
